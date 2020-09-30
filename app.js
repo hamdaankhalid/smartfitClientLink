8c6d3086019b70f7cf3ab1e50a6aa34d986ffbfc
Hamdaan Khalid <42720645+hamdaankhalid@users.noreply.github.com>
2020-09-30 11:55:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Image store
-pictures = []
+
 
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
@@ -26,23 +25,60 @@ function cameraStart() {
         }
 
 var outputcanvas = document.getElementById("output");
+var outputdummy = document.getElementById("outputdummy");
+outputdummy.getContext("2d").translate(outputdummy.width, 0);
+outputdummy.getContext("2d").scale(-1, 1);
+
 var countdown = document.getElementById("countdown");
 var time = 10;
 
+   
+// Function that scales Image on canvas
+var fitImageOn = function(canvas, imageObj) {
+	var imageAspectRatio = imageObj.width / imageObj.height;
+	var canvasAspectRatio = canvas.width / canvas.height;
+	var renderableHeight, renderableWidth, xStart, yStart;
+    var context = canvas.getContext('2d');
+	// If image's aspect ratio is less than canvas's we fit on height
+	// and place the image centrally along width
+	if(imageAspectRatio < canvasAspectRatio) {
+		renderableHeight = canvas.height;
+		renderableWidth = imageObj.width * (renderableHeight / imageObj.height);
+		xStart = (canvas.width - renderableWidth) / 2;
+		yStart = 0;
+	}
+
+	// If image's aspect ratio is greater than canvas's we fit on width
+	// and place the image centrally along height
+	else if(imageAspectRatio > canvasAspectRatio) {
+		renderableWidth = canvas.width
+		renderableHeight = imageObj.height * (renderableWidth / imageObj.width);
+		xStart = 0;
+		yStart = (canvas.height - renderableHeight) / 2;
+	}
+
+	// Happy path - keep aspect ratio
+	else {
+		renderableHeight = canvas.height;
+		renderableWidth = canvas.width;
+		xStart = 0;
+		yStart = 0;
+    }
+    
+	context.drawImage(imageObj, xStart, yStart, renderableWidth, renderableHeight);
+};
 
 function takepic(){
 
     outputcanvas.width = cameraView.videoWidth  ;
-    outputcanvas.height = cameraView.videoHeight  ;
+    outputcanvas.height = cameraView.videoHeight ;
     
     outputcanvas.getContext("2d").translate(outputcanvas.width, 0);
     outputcanvas.getContext("2d").scale(-1, 1);
 
     outputcanvas.getContext("2d").drawImage(cameraView, 0, 0);
 
-    
-    confirm.disabled = false;
-    document.getElementById('output').scrollIntoView();
+    fitImageOn(outputdummy, cameraView);
 }
   
 // Take a picture when cameraTrigger is tapped
@@ -66,18 +102,21 @@ cameraTrigger.onclick = function() {
 const redo = document.getElementById("redo").addEventListener("click",()=>{
     var outputctx =  outputcanvas.getContext("2d");
     outputctx.clearRect(0, 0, outputcanvas.width, outputcanvas.height);
+
+    var outputdummyctx =  outputdummy.getContext("2d");
+    outputdummyctx.clearRect(0, 0, outputdummy.width, outputdummy.height);
+
     time = 10;
     confirm.disabled = true;
     document.getElementById('camera--view').scrollIntoView();
 });
 
 // Save Image if confirm is clicked
-confirm.addEventListener("click",downloadPNG);
+confirm.addEventListener("click",downloadJPG);
 
 
-function downloadPNG() {
+function downloadJPG() {
     time=10;
-
     var link = document.createElement('a');
     link.download = 'smartfit.png';
     link.href = outputcanvas.toDataURL()
